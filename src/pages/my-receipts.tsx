175a7599abedbcf90caf2c3015ce1afc6b0b756b
AhmedBhiri <ahmedbhiri93@gmail.com>
2023-09-13 19:19:57 +0100

    more frontend yachraf

--- src/pages/my-receipts.tsx
+++ src/pages/my-receipts.tsx
@@ -1,8 +1,13 @@
 import MainLayout from "  /layouts/MainLayout";
 import React from "react";
 
+import InvoiceTable from "../components/my-invoices/InvoiceTable";
 const MyReceipts = () => {
-  return <MainLayout>my-receipts </MainLayout>;
+  return (
+    <MainLayout>
+      <InvoiceTable />
+    </MainLayout>
+  );
 };
 
 export default MyReceipts;
